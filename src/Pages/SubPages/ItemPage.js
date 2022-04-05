70ee95f7ab0b205c77462e31f55c6eff434e57b4
Nuclear One <ajithsnair10@gmail.com>
2022-04-05 13:29:14 +0530

    add extra (revews ,etc)

--- src/Pages/SubPages/ItemPage.js
+++ src/Pages/SubPages/ItemPage.js
@@ -1,46 +1,83 @@
 import React from "react";
-import './ItemPage.css';
+import "./ItemPage.css";
+import Reviews from "./Reviews";
 
 const ItemPage = (props) => {
   return (
     <div>
-      <div className="maindivitemPage" >
-        <h1 className="HeadingitemPage" >Album 1</h1>
+      <div className="maindivitemPage">
+        <h1 className="HeadingitemPage">Album 1</h1>
         <hr></hr>
         <div className="ImageitemPage">
-            <div className="imageitem">
-          <img
-            src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
-            alt="image"
-          />
+          <div className="multipleImageitemPage">
+            <div>
+              {" "}
+              <img
+                src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+                alt="image"
+                className="indImageitemPage"
+              />
+            </div>
+            <div>
+              {" "}
+              <img
+                src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+                alt="image"
+                className="indImageitemPage"
+              />
+            </div>
+            <div>
+              {" "}
+              <img
+                src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+                alt="image"
+                className="indImageitemPage"
+              />
+            </div>
+            <div>
+              {" "}
+              <img
+                src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+                alt="image"
+                className="indImageitemPage"
+              />
+            </div>
+          </div>
+          <div className="imageitem">
+            <img
+              src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+              alt="image"
+            />
           </div>
         </div>
-        <div  className="ContentHadnleitemPage" >
+        <div className="ContentHadnleitemPage">
           <div className="smallnameitem">
             <label> Smartees</label>
           </div>
           <div className="h3item">
             <h3>Full Sleeve Printed Men Sweatshirt</h3>
           </div>
-          <div className="colortextnameitem" >
+          <div className="colortextnameitem">
             <label>Special price</label>
           </div>
           <div className="pricedivitem">
-            <label className="amountitem" > 308 </label>
+            <label className="amountitem"> 308 </label>
             <label className="strikeitem"> $1,499</label>
-            <label className="colortextnameitem" > 79% off</label>
+            <label className="colortextnameitem"> 79% off</label>
           </div>
           <div className="ratingitem">
             <button className="Buttonratingitem">4★</button>
-            <label className="ratingcountitem">11,977 ratings and 1,413 reviews</label>
+            <label className="ratingcountitem">
+              11,977 ratings and 1,413 reviews
+            </label>
           </div>
-          <div  className="smallheadingtitem" >Coupons for you</div>
+          <div className="smallheadingtitem">Coupons for you</div>
           <div>
-              <ul>
-            <li>
-              Special PriceGet extra 30% off upto ₹50 on 1 item(s) (price
-              inclusive of discount)
-            </li>
+            <ul>
+              <li>
+                Special PriceGet extra 30% off upto ₹50 on 1 item(s) (price
+                inclusive of discount)
+              </li>
             </ul>
           </div>
           <div className="smallheadingtitem">
@@ -56,18 +93,74 @@ const ItemPage = (props) => {
             </ul>
           </div>
 
-          <div className="deliverCheckDiv" >
+          <div className="deliverCheckDiv">
             <div>
               <label className="deliverChecklabel">Deliver to</label>
-          </div>
-          <div className="deliverCheckbtninput" > 
-              <input type='text' placeholder="Enter delivery pincode" className="deliverCheckinput"/> 
+            </div>
+            <div className="deliverCheckbtninput">
+              <input
+                type="number"
+                placeholder="Enter delivery pincode"
+                className="deliverCheckinput"
+              />
               <button className="deliverCheckibtn">Check</button>
+            </div>
           </div>
+          <div className="servicesDiv">
+            <div className="serviceslabel">
+              <label>Services </label>
+            </div>
+            <div className="servicesitems">
+              <div>
+                <label>
+                  <span className="servicesitemsspan">⟳</span> 10 Hour Return
+                  Policy
+                </label>
+              </div>
+              <div>
+                <label>
+                  {" "}
+                  <span className="servicesitemsspan">❥</span> Bitcoin payment
+                  available
+                </label>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="ProductdetailsMaindiv">
+          <div className="ProductdetailsProductdiv">
+            {" "}
+            <label>Product Details</label>{" "}
+          </div>
+          <div className="Productdetailsplusdiv">
+            {" "}
+            <label>+</label>{" "}
+          </div>
+        </div>
 
-
-
+        <div className="ProductdetailsMaindiv">
+          <div>
+            <div className="ProductdetailsProductdiv">
+              {" "}
+              <label>Reviews</label>
+              <div className="reviewratingitem">
+                <button className="reviewButtonratingitem">4★</button>
+                <label className="ReviwRatingcountitem">
+                  11,977 ratings and 1,413 reviews
+                </label>
+                <div className="ReviwProductBTNDiv">
+                  <button className="ReviwProductBTN"> Rate Product</button>
+                </div>
+              </div>
+            </div>
+            <div className="Productdetailsplusdiv">
+              {" "}
+              <button className="Productdetailsplusbutton">+</button>{" "}
+            </div>
+          </div>
+        </div>
+        <div >
+          <Reviews className="Reviewbody" />
         </div>
       </div>
     </div>
---
+++ src/Pages/SubPages/Reviews.js
@@ -0,0 +1,219 @@
+import "./Reviews.css";
+
+const Reviews = () => {
+  return (
+    <div className="reviewMainDiv">
+      <div className="reviewothers">
+        <label>
+          <span className="smiley">☺</span> What our customers felt:{" "}
+        </label>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+      <div className="MainDivClass">
+        <div className="MainDivPositionClass">
+          <button className="BTNClass">4★</button>
+        </div>
+        <div className="labelreviewClass">
+          <label> Very Very good </label>
+        </div>
+        <div className="UserDetailsClass">
+          {" "}
+          <label className="UserDetailsClassName">User Name</label>{" "}
+          <label> Jan, 2021</label>{" "}
+        </div>
+        <div>
+          <div className="UserDetailsClass">
+            <label> Certified Buyer </label>
+          </div>
+          <div className="LikeClass">
+            <label>👍 366 </label> <label> 👎 60</label>{" "}
+            <label>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>{" "}
+            </label>
+          </div>
+        </div>
+        <hr className="hrclass"></hr>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
