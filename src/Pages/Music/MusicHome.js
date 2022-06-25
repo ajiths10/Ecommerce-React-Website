2371fbf9f4d2c642a659af096227bf3de2be47f4
ajiths10 <ajithsnair10@gmail.com>
2022-06-25 18:56:09 +0530

    make item dynamic

--- src/Pages/Music/MusicHome.js
+++ src/Pages/Music/MusicHome.js
@@ -1,65 +1,40 @@
+import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
+import axios from "axios";
 import "./MusicHome.css";
 
 const MusicHome = () => {
-  let productsArr = [
-    {
-      id: 1,
-      title: "Colors",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-        quantity: 1,
-    },
+  const [productsArr, setProductsArr] = useState([]);
 
-    { id:2,
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-      quantity: 1,
-    },
-
-    {
-      id: 3,
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-        quantity: 1,
-    },
-
-    {
-      id:4,
-      title: "Blue Color",
-      price: 102,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-        quantity: 1,
-    },
-  ];
   let num = 1;
-  const itemList = productsArr.map((element) => {
-    return (
-      
-        <ItemList
-          id={element.id}
-          title={element.title}
-          price={element.price}
-          URL={element.imageUrl}
-          quantity={element.quantity}
-          num={num++}
-        />
-      
-    );
-  });
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/products")
+      .then((res) => {
+        console.log(res)
+        setProductsArr(res.data.products)
+  })
+  }, []);
 
   return (
     <div className="mainBody">
       <div>
         <h2>MUSIC</h2>
       </div>
-      <div className="musiclist">{itemList}</div>
+      <div className="musiclist">
+        {productsArr.map((element) => {
+          return (
+            <ItemList
+              id={element.id}
+              title={element.title}
+              price={element.price}
+              URL={element.imageUrl}
+              quantity={1}
+              num={num++}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
