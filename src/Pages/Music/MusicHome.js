ea9c6ce7b4c2481d875d36ba22d8bfea5fcaf183
ajiths10 <ajithsnair10@gmail.com>
2022-06-26 12:24:52 +0530

    add pagination

--- src/Pages/Music/MusicHome.js
+++ src/Pages/Music/MusicHome.js
@@ -6,23 +6,33 @@ import "./MusicHome.css";
 const MusicHome = () => {
   const [productsArr, setProductsArr] = useState([]);
 
+const getProducts = async(value) => {
+  try{
+    const res = await axios.get(`http://localhost:4000?page=${value}`)
+      console.log(res);
+      setProductsArr(res.data);
+  }
+  catch(err){console.log(err)};
+}
   let num = 1;
   useEffect(() => {
-    axios
-      .get("http://localhost:4000/products")
-      .then((res) => {
-        console.log(res)
-        setProductsArr(res.data.products)
-  })
+    getProducts(1);
   }, []);
 
+  const nextProducts = () => {
+    getProducts(productsArr.nextPage)
+  }
+  const previousProducts = () => {
+    getProducts(productsArr.previousPage)
+
+  }
   return (
     <div className="mainBody">
       <div>
         <h2>MUSIC</h2>
       </div>
       <div className="musiclist">
-        {productsArr.map((element) => {
+        {productsArr?.products?.rows?.map((element) => {
           return (
             <ItemList
               id={element.id}
@@ -34,6 +44,11 @@ const MusicHome = () => {
             />
           );
         })}
+      </div>
+      <div>
+        <button onClick={previousProducts}> {'<'} </button>
+        <button>{productsArr.currentPage}</button>
+        <button onClick={nextProducts}> {'>'} </button>
       </div>
     </div>
   );
