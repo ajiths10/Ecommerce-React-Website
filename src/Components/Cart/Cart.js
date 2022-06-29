fd10222c706ae2e7bfccc5f071228e8c3102247b
ajiths10 <ajithsnair10@gmail.com>
2022-06-29 23:52:15 +0530

    Add checkout function

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,26 +2,40 @@ import React, { useContext } from "react";
 import CartContext from "../../Store/Cart--context";
 import CartList from "./CartList";
 import "./Cart.css";
+import axios from "axios";
 
 const Cart = (props) => {
-const CTX = useContext(CartContext);
+  const CTX = useContext(CartContext);
 
+  const checkoutHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:4000/checkout`);
+      console.log(response);
+      alert("sucess");
+    } catch (err) {
+      console.log(`Something went wrong!!`);
+    }
+  };
 
   const cartListItems = CTX.items.map((element) => {
-      
-    return<CartList
-      title={element.title}
-      price={element.price}
-      url={element.URL}
-      quantity={element.quantity} 
-      id={element.id}
-    />;
+    return (
+      <CartList
+        title={element.title}
+        price={element.price}
+        url={element.URL}
+        quantity={element.quantity}
+        id={element.id}
+      />
+    );
   });
-  
+
   return (
     <div className="cartBody">
       <div className="heading-cart">
-      <button className="closeButton-cart" onClick={props.onClick} >X</button>
+        <button className="closeButton-cart" onClick={props.onClick}>
+          X
+        </button>
         <h1> Cart</h1>
       </div>
       <div>
@@ -32,8 +46,10 @@ const CTX = useContext(CartContext);
       </div>
       <div>{cartListItems}</div>
       <div className="totalAmount">
-          <span >Total Amount:{CTX.total} </span>
-          <button className="purchaseBtn" >PURCHASE</button>
+        <span>Total Amount:{CTX.total} </span>
+        <button className="purchaseBtn" onClick={checkoutHandler}>
+          Checkout
+        </button>
       </div>
     </div>
   );
