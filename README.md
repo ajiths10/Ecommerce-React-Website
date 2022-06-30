2dfc5ad8f3b9f8e91cef0f89b2b00704c5bf1a6f
ajiths10 <ajithsnair10@gmail.com>
2022-06-30 22:58:18 +0530

    Style and readme update

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ check it out:
 
 https://ecommercewebsite-98a99.web.app
 
+use this for backend  - https://github.com/ajiths10/Node-MVC
 
- 
+Take a clone - https://github.com/ajiths10/Node-MVC.git
--- src/Components/Orders/ListView.js
+++ src/Components/Orders/ListView.js
@@ -6,7 +6,7 @@ const ListView = (props) => {
 
   return (
     <div className="listView-container">
-      <img src={props.data.imageUrl} alt="img" />
+      <img className="img" src={props.data.imageUrl} alt="img" />
       <h3> {props.data.title} </h3>
       <label> quantity: <span>{props.data.customcheckout.quantity}</span> </label>
       <label>  Amount: <span> ${props.data.price}</span> </label>
